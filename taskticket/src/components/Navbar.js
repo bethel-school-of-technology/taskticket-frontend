bf00ac488d7d88f0a2d6b5853e180178ef5c1a78
Mandee Miller <mandee.l.miller@gmail.com>
2019-08-27 22:51:34 -0400

    updated navbar

--- taskticket/src/components/Navbar.js
+++ taskticket/src/components/Navbar.js
@@ -3,22 +3,16 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <nav className="main-nav">
-                <ul>
-                    <li>
-                        <a href="/">Logo</a>
-                    </li>
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="/">Sign Up</a>
-                    </li>
-                    <li>
-                        <a href="/">Logout</a>
-                    </li>
-                </ul>
-            </nav>
+            <div>
+                <nav className="navigation">
+                    <ul className="menu">
+                        <li><a href="/">Logo</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li className="signup"><a href="/">Sign Up</a></li>
+                        <li className="signup"><a href="/">Login</a></li>
+                    </ul>
+                </nav>
+            </div>
         )
     }
 }
