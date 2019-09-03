c76d1457cd7d61c409c78a8265b2ce9d260735e0
Mandee Miller <mandee.l.miller@gmail.com>
2019-09-03 16:05:19 -0400

    about.page

--- taskticket/src/components/Home.js
+++ taskticket/src/components/Home.js
@@ -3,9 +3,58 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return (
-            <h1 className="App__Name">About</h1>
-            
-            
+            <div className="Home_Page">
+                <h1 className="App__Name">About</h1>
+                <div>
+                    <p className="aboutP">We started by saying YES to God!
+                    Placed together, by no accident,
+                    4 team members embark on a journey of development-
+                    through coding and internally.
+                    Patience and dedication were tested.
+                    Emotions of all kinds were to arise, but in the end,
+                    WE prevailed. WE are developers, WE are qualified,
+                but most importantly WE are family!</p>
+                </div>
+
+                <hr></hr>
+
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+                <div className="Ind_About">
+                    <div className="Mandee_About">
+                        <h3>Mandee Miller</h3>
+                        <a target="_blank" href="https://www.facebook.com/mandee.miller.9" class="fa fa-facebook"></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/mandee-miller-35a99b185/" class="fa fa-linkedin"></a>
+                        <a target="_blank" href="https://github.com/mandeelaraye" class="fa fa-github"></a>
+                    </div>
+
+
+
+                    <div className="Shannon_About">
+                        <h3>Shannon Wolff</h3>
+                        <a target="_blank" href="https://www.facebook.com/shewolff182" class="fa fa-facebook"></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/shewolff182/" class="fa fa-linkedin"></a>
+                        <a target="_blank" href="https://github.com/Shewolff182" class="fa fa-github"></a>
+                    </div>
+
+                </div>
+
+                <div className="Ind_About">
+                    <div className="Vali_About">
+                        <h3>Vali Masca</h3>
+                        <a target="_blank" href="https://www.facebook.com/laurean.valer" class="fa fa-facebook"></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/vali-l-masca-06568511b/" class="fa fa-linkedin"></a>
+                        <a target="_blank" href="https://github.com/valimasca" class="fa fa-github"></a>
+                    </div>
+
+                    <div className="James_About">
+                        <h3>James Ramsey</h3>
+                        <a target="_blank" href="https://github.com/ramseyman86" class="fa fa-github"></a>
+                    </div>
+                </div>
+
+
+            </div>
         )
     }
 }
