57031c409cb475a893ce0694ac9a7247e1a9890b
Valer Masca <e3skadron@gmail.com>
2019-09-10 16:05:03 -0700

    commentss....

--- taskticket/src/components/RequestList.js
+++ taskticket/src/components/RequestList.js
@@ -1,14 +1,21 @@
-import React, {Component} from 'react';
- 
+import React, { Component } from 'react';
+
 class RequestList extends Component {
     // componentDidLoad(){
     //     //do fetch to get one 
     //     this.setState
     // }
-    render () {
+    render() {
         return (
-            <div className= "RequestList">
-                <p>This is the List page</p>
+            <div className="RequestList">
+                <h3>Request List</h3>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="http://localhost:4000/request">Click me</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
         );
--- taskticket/src/components/RequestView.js
+++ taskticket/src/components/RequestView.js
@@ -5,12 +5,18 @@ class RequestView extends Component {
     //     //do fetch to get one 
     //     this.setState
     // }
-    render () {
+    render() {
         return (
-            <div className= "RequestView">
-                <p>This is the View page</p>
+            <div className="RequestView">
+                <h3>Welcome to View Page</h3>
+                <p>Please read the comments carefully and be respectful!!!</p>
+                <form>
+                    <p>Display message with a certain id</p>
+                    <textarea>Comment here</textarea>
+                    <button>Submit your kind comment</button>
+                </form>
             </div>
-           
+
         );
     }
 }
