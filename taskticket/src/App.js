c0ef762cda2cfb9b85da6381fbc0d2ee5ca501df
Valer Masca <e3skadron@gmail.com>
2019-09-15 09:18:05 -0700

    request list sorted

--- taskticket/src/App.js
+++ taskticket/src/App.js
@@ -7,7 +7,7 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 import RequestPages from './components/RequestPages';
 import RequestForm from './components/RequestForm';
-import RequestList from './components/RequestList';
+import Requests from './components/RequestList';
 import RequestView from './components/RequestView';
 import ErrorHandler from './components/ErrorHandler';
 import Navbar from './components/Navbar';
@@ -18,28 +18,28 @@ import UserProfile from './components/UserProfile';
 class App extends Component {
   render() {
     return (
-        <Router>
-          <ErrorHandler>
-            <Navbar />
-            <div className="App">
-              <div className="App__Aside"><Route path="/" component={Header} /></div>
-              <div className="App__Form">
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/signUp" component={SignUp} />
-                <Route path="/requests" component={RequestPages} />
-                <Route path="/requests/form" component={RequestForm} />
-                <Route path="/requests/list" component={RequestList} />
-                <Route path="/requests/view" component={RequestView} />
-                <Route path="/userProfile" component={UserProfile} />
-              </div>
+      <Router>
+        <ErrorHandler>
+          <Navbar />
+          <div className="App">
+            <div className="App__Aside"><Route path="/" component={Header} /></div>
+            <div className="App__Form">
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/signUp" component={SignUp} />
+              <Route path="/requests" component={RequestPages} />
+              <Route path="/requests/form" component={RequestForm} />
+              <Route path="/requests/list" component={Requests} />
+              <Route path="/requests/view" component={RequestView} />
+              <Route path="/userProfile" component={UserProfile} />
             </div>
-            <Footer />
-          </ErrorHandler>
-        </Router>
-        
+          </div>
+          <Footer />
+        </ErrorHandler>
+      </Router>
+
     );
   }
 }
-    
+
 export default App;
--- taskticket/src/components/RequestList.js
+++ taskticket/src/components/RequestList.js
@@ -1,97 +1,148 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-let APIUrl = "http://localhost:4000/request/list";
+//let APIUri = "http://localhost:4000/request";
 
-class Request extends Component {
-    state = {
-        comments: [],
-        errors: null
-    }
-    // componentDidMount() {
-    //     fetch(APIUrl)
-    //         .then(rep => rep.json())
-    //         .then(payload => {
-    //             let loadedComments = payload.filter((i, index) => index < 50);
-    //             console.log(loadedComments);
-    //             this.setState({ comments: loadedComments })
-    //         })
-    //         .catch(err => this.setState({ error: err }));
-    // }
+class Requests extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            requestData: [],
+            errors: null
 
         };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.fetchrequestData = this.fetchrequestData.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
-    handleSubmit() {
-        const { comments } = this.state;
+    fetchrequestData = () => {
         const apiUrl = "http://localhost:4000/request";
-        return axios.post(apiUrl, {
-            comments
+        return axios.get(apiUrl).then(response => {
+            this.setState(() => {
+                return { requestData: response.data };
+            });
+        });
+    }
 
-        });
+    componentDidMount() {
+        this.fetchrequestData();
     }
-    handleChange(key, event) {
-        this.setState({
-            [key]: event.target.value
-        });
-    }
+
     render() {
-        const { loadedComments } = this.state;
+        console.log(this.state.requestData);
+        if (this.state.requestData.length === 0) {
+            return (<div>Failed to fetch data from server</div>);
+        }
 
-        return (
-            <React.Fragment>
-                <form className="FormFields">
-                    <div className="FormField">
-                        <label className="FormField__Label" htmlFor="item">Request List: </label><br></br>
-                        <p>This is where the list of all comments will be</p>
-                        {/* {this.state.comments.map(
-
-                        )} */}
-                    </div>
-
-                </form>
-            </React.Fragment>
-        )
+        const requests = this.state.requestData.map(request => (
+            <div key={request.item}>
+                <a href="http://localhost:4000/request">{request.item}</a> : {request.description}
+            </div>
+        ));
+        return (<div>{requests}</div>);
     }
 }
 
-export default Request;
+export default Requests;
 
 
 
-// class RequestList extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state= {
-//             comment: "",
-//             author: ""
-//         };    
+
+
+
+// import React, { Component } from 'react';
+// import axios from 'axios';
+
+// let APIUrl = "http://localhost:4000/request/list";
+
+// class Request extends Component {
+//     state = {
+//         comments: [],
+//         errors: null
 //     }
-
-//     // componentDidLoad(){
-//     //     //do fetch to get one 
-//     //     this.setState
+//     // componentDidMount() {
+//     //     fetch(APIUrl)
+//     //         .then(rep => rep.json())
+//     //         .then(payload => {
+//     //             let loadedComments = payload.filter((i, index) => index < 50);
+//     //             console.log(loadedComments);
+//     //             this.setState({ comments: loadedComments })
+//     //         })
+//     //         .catch(err => this.setState({ error: err }));
 //     // }
-//     render() {
-//         return (
-//             <div className="RequestList">
-//                 <h3>Request List</h3>
-//                 <div>
-//                     <ul>
-//                         <li>
-//                             <a href="http://localhost:4000/request">Click me</a>
-//                         </li>
-//                     </ul>
-//                 </div>
-//             </div>
+//     constructor(props) {
+//         super(props);
+//         this.state = {
 
-//         );
+
+//         };
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//     handleSubmit() {
+//         const { comments } = this.state;
+//         const apiUrl = "http://localhost:4000/request";
+//         return axios.post(apiUrl, {
+//             comments
+
+//         });
+//     }
+//     handleChange(key, event) {
+//         this.setState({
+//             [key]: event.target.value
+//         });
+//     }
+//     render() {
+//         const { loadedComments } = this.state;
+
+//         return (
+//             <React.Fragment>
+//                 <form className="FormFields">
+//                     <div className="FormField">
+//                         <label className="FormField__Label" htmlFor="item">Request List: </label><br></br>
+//                         <p>This is where the list of all comments will be</p>
+//                         {/* {this.state.comments.map(
+
+//                         )} */}
+//                     </div>
+
+//                 </form>
+//             </React.Fragment>
+//         )
 //     }
 // }
 
-// export default RequestList;
+// export default Request;
+
+
+
+// // class RequestList extends Component {
+// //     constructor(props){
+// //         super(props);
+// //         this.state= {
+// //             comment: "",
+// //             author: ""
+// //         };    
+// //     }
+
+// //     // componentDidLoad(){
+// //     //     //do fetch to get one 
+// //     //     this.setState
+// //     // }
+// //     render() {
+// //         return (
+// //             <div className="RequestList">
+// //                 <h3>Request List</h3>
+// //                 <div>
+// //                     <ul>
+// //                         <li>
+// //                             <a href="http://localhost:4000/request">Click me</a>
+// //                         </li>
+// //                     </ul>
+// //                 </div>
+// //             </div>
+
+// //         );
+// //     }
+// // }
+
+// // export default RequestList;
